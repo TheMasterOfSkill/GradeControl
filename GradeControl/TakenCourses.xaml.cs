6f2c0b49a88d16ccd25b0106950c30b4df2e0a9d
Fabrice von der Lehr <fabrice.vonderlehr222@googlemail.com>
2017-12-26 17:48:23 +0100

    TakenCourses GUI

--- GradeControl/TakenCourses.xaml.cs
+++ GradeControl/TakenCourses.xaml.cs
@@ -10,25 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GradeControl
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für TakenCourses.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TakenCourses : Window
     {
-        public MainWindow()
+        public TakenCourses()
         {
             InitializeComponent();
         }
 
-        public void OpenTakenCourses(object sender, RoutedEventArgs e)
-        {
-            TakenCourses takenCourses = new TakenCourses();
-            takenCourses.Show();
-        }
+
     }
 }
